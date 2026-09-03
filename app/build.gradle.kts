509fd0db583a924703284798bc5eb5dfd91d3551
loc ❤️😼 <loc.nguyen2@vietunion.com.vn>
2026-09-03 17:38:46 +0700

    build: increment versionCode to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.tatl.fastnote"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
+        versionCode = 18
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
