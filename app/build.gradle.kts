d37f9a41c6f3c62ad003834fba1e4f53c42b0530
LOC\locn0 <locn049@gmail.com>
2026-09-07 21:04:45 +0700

    build: bump versionCode to 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.tatl.fastnote"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
+        versionCode = 22
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
